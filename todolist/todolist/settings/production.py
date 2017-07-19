9999a97453523fd467c2529833a67acd7ea51bb6
Erik Ferlanti <eferlanti@tacc.utexas.edu>
2017-07-19 16:53:45 -0500

    adding django project

--- todolist/todolist/settings/production.py
+++ todolist/todolist/settings/production.py
@@ -1,10 +1,7 @@
-# Parse database configuration from $DATABASE_URL
 import dj_database_url
 from os import environ
-
 from .base import *
 
-ALLOWED_HOSTS = ['yourproject.example.com']
 
 DATABASES = {
     'default': dj_database_url.config()
